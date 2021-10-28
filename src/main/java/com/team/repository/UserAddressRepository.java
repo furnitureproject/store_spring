aa5a9bd0ead9cd68a2b574e2c1a880a752c9daf9
Ymir-niflheim <85853130+Ymir-niflheim@users.noreply.github.com>
2021-10-28 17:37:22 +0900

    강건호20211028
    useraddress부분 projection수정,,repository 내용 추가

--- src/main/java/com/team/repository/UserAddressRepository.java
+++ src/main/java/com/team/repository/UserAddressRepository.java
@@ -1,6 +1,7 @@
 package com.team.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.team.entity.UserAddress;
 import com.team.entity.UserAddressProjection;
@@ -12,5 +13,7 @@ import org.springframework.stereotype.Repository;
 public interface UserAddressRepository extends JpaRepository<UserAddress, Long> {
 
     List<UserAddressProjection> findByUser_UserId(String userId);
+
+    Optional<UserAddressProjection> findByAddressNo(long addressNo);
 
 }
