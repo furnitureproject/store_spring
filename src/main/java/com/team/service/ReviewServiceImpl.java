4a5549a79bf01db3c101d34badbf925db15d2978
fkrkaktk <68282411+fkrkaktk@users.noreply.github.com>
2021-11-01 14:45:50 +0900

    김원준
    review 수정

--- src/main/java/com/team/service/ReviewServiceImpl.java
+++ src/main/java/com/team/service/ReviewServiceImpl.java
@@ -8,7 +8,9 @@ import com.team.entity.ReviewProjection;
 import com.team.repository.ReviewRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ReviewServiceImpl implements ReviewService {
 
     @Autowired
